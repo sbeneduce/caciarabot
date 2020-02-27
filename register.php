784ade0c4b1c5ffa4772a807775495adee2a7405
Sergio Beneduce <sbeneduce@gmail.com>
2020-02-27 10:36:56 +0100

    Add files via upload

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://caciarabot.herokuapp.com/execute.php';
+$BOT_TOKEN = '609433894:AAHB55vHwngtFzAHpfkqqqCspUV4BqBVa7c';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
